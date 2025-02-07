14d1f39e34d6d6b4947f4180b4a55c13a49b0c88
Shlyonskiy Anton <sh.cikada@gmail.com>
2025-02-08 00:08:15 +0300

    refact: separate methods; fix magic literals

--- src/functions/TriangleDemo.java
+++ src/functions/TriangleDemo.java
@@ -1,3 +1,5 @@
+package functions;
+
 /*
  * Неделя 1 (Computer Science, функции)
  * Домашнее задание "Условные операторы".
@@ -8,7 +10,8 @@ public class TriangleDemo {
     public static void main(String[] args) {
         Triangle tr1 = new Triangle(5, 10);
         Triangle tr2 = new Triangle(10, 20);
-        //setVolume() выполняется в конструкторе класса Triangle
-        Triangle.compare(tr1, tr2);
+
+        //setVolume() выполняется в конструкторе класса functions.Triangle
+        tr1.printCompareVolume(tr2);
     }
 }
